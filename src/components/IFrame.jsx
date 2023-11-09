6fa59c95fac76d39394dcf850c43a7a9afd0e467
Arnaud Leduc <arnaud.leduc3377@gmail.com>
2023-11-09 13:05:39 +0100

    Added vercel dependency and vercel.json to configure routes

--- src/components/IFrame.jsx
+++ src/components/IFrame.jsx
@@ -1,14 +1,7 @@
 import { Html } from "@react-three/drei"
 import '../style/iframe.css'
-import { useControls } from "leva"
 
 export default function IFrame() {
-    const { x, y, z} = useControls({
-        x: 0,
-        y: 0,
-        z: 0
-    })
-
     return (
         <Html
             transform
