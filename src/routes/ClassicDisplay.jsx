3bc1b74921aa15f41fd42dfb6748d3a72a60ae8b
Arnaud Leduc <arnaud.leduc3377@gmail.com>
2023-12-18 10:36:42 +0100

    Added translations and updates to responsive version

--- src/routes/ClassicDisplay.jsx
+++ src/routes/ClassicDisplay.jsx
@@ -1,11 +1,14 @@
 import "../style/classic-display.css";
+import { useTranslation } from "react-i18next";
 
 export default function ClassicDisplay() {
+  const { t } = useTranslation();
+
   return (
     <div className="classic-display">
       <h1>Arnaud LEDUC</h1>
-      <h2>Creative Developer</h2>
-      <p>Creative developer and 3D graphic designer living in Agen, France</p>
+      <h2>{t("creative")}</h2>
+      <p>{t("pitch")}</p>
 
       <h3>CONTACT</h3>
       <ul className="classic-display-list-container">
@@ -18,36 +21,40 @@ export default function ClassicDisplay() {
         <li>
           <a href="https://github.com/arnaudleduc">GITHUB</a>
         </li>
+        <li>
+          <a href="mailto:arnaud.leduc3377@gmail.com">EMAIL ADDRESS</a>
+        </li>
       </ul>
 
-      <h3>EXPERIENCES</h3>
+      <h3>{t("experiences")}</h3>
       <h4>
-        FREELANCER / CREATIVE DEVELOPER <span>AT </span>ZDK WEB AGENCY
+        {t("freelance")} <span>{t("at")} </span>ZDK WEB AGENCY
       </h4>
       <p>
-        2023<span>&gt;</span>Aujourd&apos;hui
+        2023<span>&gt;</span>
+        {t("today")}
       </p>
       <h4>
-        FRONTEND DEVELOPER <span>AT </span>SKILLEOS
+        {t("frontend")} <span>{t("at")} </span>SKILLEOS
       </h4>
       <p>
         2022<span>&gt;</span>2023
       </p>
       <h4>
-        CEO / 3D GRAPHIC DESIGNER <span>AT </span>TROISIEME ANGLE
+        {t("designer")} <span>{t("at")} </span>TROISIEME ANGLE
       </h4>
       <p>
         2018<span>&gt;</span>2019
       </p>
       <h4>
-        GRAPHIC DESIGNER <span>AT </span>MAESTRO CORPORATION
+        {t("designer")} <span>{t("at")} </span>MAESTRO CORP.
       </h4>
       <p>
         2017<span>&gt;</span>2018
       </p>
 
-      <h3>PROJECTS</h3>
-      <h4>IMMERSIVE WEBSITES</h4>
+      <h3>{t("projects")}</h3>
+      <h4>{t("immersive")}</h4>
       <ul className="classic-display-list-container immersive-websites">
         <li>
           <a href="https://abandoned-house.vercel.app/">
@@ -58,19 +65,19 @@ export default function ClassicDisplay() {
           <a href="https://zdk-web-agency.vercel.app/">ZDK WEB AGENCY</a>
         </li>
       </ul>
-      <h4>COMMON WEBSITES</h4>
+      <h4>{t("common")}</h4>
       <ul className="classic-display-list-container">
         <li>
           <a href="https://www.skilleos.com/">SKILLEOS</a>
         </li>
       </ul>
-      <h4>GRAPHIC DESIGN</h4>
+      <h4>{t("design")}</h4>
       <ul className="classic-display-list-container">
         {/* <li>
           <a href="https://www.behance.net/arnaudleduc">BEHANCE</a>
         </li> */}
         <li>
-          <a href="https://www.artstation.com/zdk">ARTSTATIoN</a>
+          <a href="https://www.artstation.com/zdk">ARTSTATION</a>
         </li>
       </ul>
     </div>
